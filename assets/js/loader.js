b4ab24fdc8a3a06529ba58505725090c24071c45
Niyoj Oli <oliniyoj@gmail.com>
2022-10-07 12:59:28 +0545

    feat: completes loader for website

---
+++ assets/js/loader.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let loader = document.getElementById("site-loader");
+    let content = document.getElementById("wrapper");
+
+    loader.style.display = "none";
+    content.style.display = "block";
+};
